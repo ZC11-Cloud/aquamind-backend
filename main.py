20170a1db0dad4a8aa0d7055333bab6cf34719f5
zhangchi <3368317441@qq.com>
2026-01-07 15:49:03 +0800

    feat(main):新增中间件CORSMiddleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.user import router as user_router
 
 
@@ -11,6 +12,16 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()]
 )
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user_router)
 
 @app.get("/")
